8bbf08b70f74260bf57a6fbcc24be80992e58188
misakitsubota <misakitsubota@icloud.com>
2020-11-16 12:40:51 +0900

    トップページの作成とユーザー管理機能バリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,33 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :products
+  # has_many :buyers
+
+
+  NAME_REGEX = /\A[ぁ-んァ-ン一-龥]+\z/
+  NAME_KANA_REGEX = /\A[ァ-ヶー－]+\z/
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+
+
+  with_options presence: true, format: { with: NAME_REGEX, message: 'is invalid. input full-width characters.' } do
+    validates :first_name
+    validates :family_name
+  end
+
+  with_options presence: true, format: { with: NAME_KANA_REGEX, message: 'is invalid. input full-width katakana characters.' } do
+    validates :first_name_kana
+    validates :family_name_kana
+  end
+
+  with_options presence: true do
+    validates :nickname
+    validates :email
+    validates :birth_day
+  end
+
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
+
 end
+
