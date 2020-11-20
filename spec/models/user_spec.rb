4f228c6afffd393777b36d0182fe41af42db2df4
misakitsubota <misakitsubota@icloud.com>
2020-11-20 09:15:05 +0900

    テストコードの追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -40,6 +40,11 @@ describe User do
         another_user.valid?
         expect(another_user.errors.full_messages).to include('Email has already been taken')
       end
+      it 'emailに＠が入っていなければ登録できない' do
+        @user.email = 'testtest'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Email is invalid')
+      end
       it 'passwordが空では登録できない' do
         @user.password = ''
         @user.password_confirmation = ''
