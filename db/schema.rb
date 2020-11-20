1993ea82e394be4c2fe8202fd280729139888b57
misakitsubota <misakitsubota@icloud.com>
2020-11-20 19:11:53 +0900

    テーブルへのカラムの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_08_043836) do
+ActiveRecord::Schema.define(version: 2020_11_20_095324) do
+
+  create_table "products", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "product_name", null: false
+    t.text "description", null: false
+    t.integer "cost", null: false
+    t.integer "product_category_id", null: false
+    t.integer "product_condition_id", null: false
+    t.integer "delivery_fee_id", null: false
+    t.integer "prefecture_id", null: false
+    t.integer "arrival_date_id", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
