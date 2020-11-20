480329b6f09446e79ae9d0d6f31877549fe38831
misakitsubota <misakitsubota@icloud.com>
2020-11-20 13:19:09 +0900

    テストコード追加とコメントアウト削除

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -85,20 +85,40 @@ describe User do
         @user.valid?
         expect(@user.errors.full_messages).to include("Family name can't be blank", 'Family name is invalid. input full-width characters.')
       end
+      it 'family_nameが半角では登録できない' do
+        @user.family_name = 'yamada'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Family name is invalid. input full-width characters.")
+      end
       it 'first_nameが空では登録できない' do
         @user.first_name = ''
         @user.valid?
         expect(@user.errors.full_messages).to include("First name can't be blank", 'First name is invalid. input full-width characters.')
+      end
+      it 'first_nameが半角では登録できない' do
+        @user.first_name = 'tarou'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("First name is invalid. input full-width characters.")
       end
       it 'family_name_kanaが空では登録できない' do
         @user.family_name_kana = ''
         @user.valid?
         expect(@user.errors.full_messages).to include("Family name kana can't be blank", 'Family name kana is invalid. input full-width katakana characters.')
       end
+      it 'family_name_kanaが半角では登録できない' do
+        @user.family_name_kana = 'yamada'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Family name kana is invalid. input full-width katakana characters.")
+      end
       it 'first_name_kanaが空では登録できない' do
         @user.first_name_kana = ''
         @user.valid?
         expect(@user.errors.full_messages).to include("First name kana can't be blank", 'First name kana is invalid. input full-width katakana characters.')
+      end
+      it 'first_name_kanaが半角では登録できない' do
+        @user.first_name_kana = 'yamada'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("First name kana is invalid. input full-width katakana characters.")
       end
       it 'birth_dayが空では登録できない' do
         @user.birth_day = ''
