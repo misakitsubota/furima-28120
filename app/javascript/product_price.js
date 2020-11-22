3abd037d8bc7787f0ebc206a56754d68571068a5
misakitsubota <misakitsubota@icloud.com>
2020-11-22 21:19:14 +0900

    アソシエーション・バリデーションの記述変更

--- app/javascript/product_price.js
+++ app/javascript/product_price.js
@@ -2,12 +2,12 @@ window.addEventListener('load', () => {
   const priceInput = document.getElementById("product-price");
   priceInput.addEventListener("input", () => {
     const inputValue = priceInput.value;
+    const valueResult = inputValue * 0.1
     
     const addTaxDom = document.getElementById("add-tax-price");
-    addTaxDom.innerHTML = (Math.floor(inputValue * 0.1))
+    addTaxDom.innerHTML = (Math.floor(valueResult))
 
     const profitNumber = document.getElementById("profit");
-    const valueResult = inputValue * 0.1
     profitNumber.innerHTML = (Math.floor(inputValue - valueResult));
   })
 });
