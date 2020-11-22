c9bc453f12e845456485978591664624ee785e71
misakitsubota <misakitsubota@icloud.com>
2020-11-22 19:16:54 +0900

    javascript編集

--- app/javascript/product_price.js
+++ app/javascript/product_price.js
@@ -2,8 +2,13 @@ window.addEventListener('load', () => {
   const priceInput = document.getElementById("product-price");
   priceInput.addEventListener("input", () => {
     const inputValue = priceInput.value;
+    
     const addTaxDom = document.getElementById("add-tax-price");
-    addTaxDom.innerHTML = "(InputValue * 0.1)"
+    addTaxDom.innerHTML = (Math.floor(inputValue * 0.1))
+
+    const profitNumber = document.getElementById("profit");
+    const valueResult = inputValue * 0.1
+    profitNumber.innerHTML = (Math.floor(inputValue - valueResult));
   })
 });
 
