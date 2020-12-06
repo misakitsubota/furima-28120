8cf5db663b7006d81438d157acfe2ba8918a439f
misakitsubota <misakitsubota@icloud.com>
2020-12-06 18:02:57 +0900

    購入情報保存機能実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,18 +1,18 @@
 class OrdersController < ApplicationController
-  before_action :set_product,  only: [:index]
+  before_action :set_product,  only: [:index, :create]
 
   def index
     @order = ProductTransaction.new
   end
+
   
   def create
-    binding.pry
     @order = ProductTransaction.new(order_params)
     if @order.valid?
       @order.save
-      redirect_to action: :index
+      redirect_to root_path
     else
-      redirect_to product_orders_path
+      render :index
     end
   end
   
@@ -20,8 +20,9 @@ class OrdersController < ApplicationController
   def order_params
     params.require(:product_transaction).permit(:zip_code, :prefecture_id, :city, :address, :building, :phone_number, :order_id).merge(user_id: current_user.id, product_id: params[:product_id])
   end
+
+  def set_product
+    @product = Product.find(params[:product_id])
+  end
 end
 
-def set_product
-  @product = Product.find(params[:product_id])
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_05_085612) do
+ActiveRecord::Schema.define(version: 2020_12_05_060725) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -36,11 +36,6 @@ ActiveRecord::Schema.define(version: 2020_12_05_085612) do
   create_table "orders", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id", null: false
     t.integer "product_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "product_transactions", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
