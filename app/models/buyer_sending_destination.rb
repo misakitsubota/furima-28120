9cf2c905bd326c7378d957a2f5bec72e8520168d
misakitsubota <misakitsubota@icloud.com>
2020-12-09 17:27:35 +0900

    Revert "sending_destinationバリデーション設定"
    This reverts commit dfb0e178098110dd86ec2cef703a665899679217.

--- app/models/buyer_sending_destination.rb
+++ app/models/buyer_sending_destination.rb
@@ -1,6 +1,2 @@
 class BuyerSendingDestination < ApplicationRecord
-  belongs_to :buyer 
-
-  validates :zipcode, :city, :adress, :phone_number, presence: true
-  validates :building, null: true
 end
